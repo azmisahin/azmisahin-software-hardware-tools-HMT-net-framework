43bb3584791404135adf800faa3cd52d858f1e5f
Azmi ŞAHİN <azmisahin@outlook.com>
2019-07-18 10:47:31 +0300

    HMT unit test project added

---
+++ src/HMT/HMT.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("HMT Tests")]
+[assembly: AssemblyDescription("A management tool that provides easy access to hardware interfaces to automate management tasks on local or remote computers.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("HMT.Tests")]
+[assembly: AssemblyCopyright("azmisahin.com ©  2019")]
+[assembly: AssemblyTrademark("azmisahin")]
+[assembly: ComVisible(true)]
+[assembly: Guid("83af1f27-d97a-4bc5-927f-39ad303a6e7a")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
