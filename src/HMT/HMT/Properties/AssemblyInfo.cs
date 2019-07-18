2bc6189a5ab9ecdc9620c523678ee34c05913790
Azmi ŞAHİN <azmisahin@outlook.com>
2019-07-18 10:34:12 +0300

    Project "launched with the .NET Framework 4"

---
+++ src/HMT/HMT/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("HMT")]
+[assembly: AssemblyDescription("A management tool that provides easy access to hardware interfaces to automate management tasks on local or remote computers.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("HMT")]
+[assembly: AssemblyCopyright("azmisahin.com ©  2019")]
+[assembly: AssemblyTrademark("azmisahin")]
+[assembly: ComVisible(true)]
+[assembly: Guid("75f69765-b186-4b81-868a-33f051e4b336")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
